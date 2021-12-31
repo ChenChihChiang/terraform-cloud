42a26614591ace98a18329f3d2183983e84afc88
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 20:56:30 +0800

    t

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
-resource "aws_route53_zone" "aws_chenchihchiang_tw" {
-  name = "aws.chenchihchiang.tw"
+resource "aws_route53_zone" "dev_chenchihchiang_tw" {
+  name = "dev.chenchihchiang.tw"
 
   tags = {
     Terraform = "true"
