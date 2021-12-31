af84faa5ae4a47c21c40266b0b620f6da53208ec
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 21:24:16 +0800

    t

--- acm.tf
+++ acm.tf
@@ -4,9 +4,6 @@ data "aws_acm_certificate" "chenchihchiang_tw" {
   most_recent = true
 }
 
-
-
-
 resource "aws_acm_certificate" "dev_chenchihchiang_tw" {
   domain_name       = "*.dev.chenchihchiang.tw"
   validation_method = "DNS"
@@ -19,4 +16,26 @@ resource "aws_acm_certificate" "dev_chenchihchiang_tw" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_route53_record" "dev_chenchihchiang_tw" {
+  for_each = {
+    for dvo in aws_acm_certificate.dev_chenchihchiang_tw.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.dev_chenchihchiang_tw.zone_id
+}
+
+resource "aws_acm_certificate_validation" "dev_chenchihchiang_tw" {
+  certificate_arn         = aws_acm_certificate.dev_chenchihchiang_tw.arn
+  validation_record_fqdns = [for record in aws_route53_record.dev_chenchihchiang_tw : record.fqdn]
 }
