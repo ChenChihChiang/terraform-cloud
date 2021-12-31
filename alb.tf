e13df7db9f35b74c202bad4148d9367048c55e29
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 16:28:35 +0800

    t

--- alb.tf
+++ alb.tf
@@ -24,5 +24,22 @@ resource "aws_lb_target_group" "nginx_tg" {
     Terraform = "true"
     Environment = "dev"
   }
+}
 
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.chenchihchiang_tw.id
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+  }
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
