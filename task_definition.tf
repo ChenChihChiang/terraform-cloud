23ee42e302bda6abc57daa426f039bcfe17e608a
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 20:17:14 +0800

    t

--- task_definition.tf
+++ task_definition.tf
@@ -2,8 +2,6 @@ resource "aws_ecs_task_definition" "nginx" {
   family                   = "nginx"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
   task_role_arn            = "arn:aws:iam::312490145519:role/ecsTaskExecutionRole"
   execution_role_arn       = "arn:aws:iam::312490145519:role/ecsTaskExecutionRole"
   container_definitions    = <<TASK_DEFINITION
