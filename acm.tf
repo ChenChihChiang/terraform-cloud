24e7cc9330b0ef14b1578aae4eb93bb26523b118
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 21:18:04 +0800

    t

--- acm.tf
+++ acm.tf
@@ -3,3 +3,20 @@ data "aws_acm_certificate" "chenchihchiang_tw" {
   types       = ["AMAZON_ISSUED"]
   most_recent = true
 }
+
+
+
+
+resource "aws_acm_certificate" "dev_chenchihchiang_tw" {
+  domain_name       = "*.dev.chenchihchiang.tw"
+  validation_method = "DNS"
+
+  tags = {
+    Terraform = "true"
+    Environment = "test"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
