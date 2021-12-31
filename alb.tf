dc6d2c41581a77eaef59a0d1d8c5d5c901320396
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 15:42:14 +0800

    t

--- alb.tf
+++ alb.tf
@@ -12,3 +12,16 @@ resource "aws_lb" "dev_alb" {
     Environment = "dev"
   }
 }
+
+resource "aws_lb_target_group" "nginx_tg" {
+  name     = "nginx_tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.dev_vpc.vpc_id
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+
+}
