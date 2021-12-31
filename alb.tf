78a7d961a567ee7c086c02dfb450e53f35aaeb99
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 14:11:12 +0800

    t

--- alb.tf
+++ alb.tf
@@ -3,9 +3,9 @@ resource "aws_lb" "dev_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_tls.id]
-  subnets            = [for subnet in module.dev_vpc.public : subnet.id]
+  subnets            = [module.dev_vpc.public_subnets]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
     Terraform = "true"
