5322496a7543f825b733472576960d4a5fddec6d
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 14:20:54 +0800

    t

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "dev_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_tls.id]
-  subnets            = [for subnet in module.dev_vpc.public_subnets : subnet.id]
+  subnets            = [for subnet in module.dev_vpc.public_subnets]
 
   enable_deletion_protection = false
 
