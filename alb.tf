3cc8a81139ed4c99e85a8d09a8071605bc9dbf8b
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 14:07:49 +0800

    t

--- alb.tf
+++ alb.tf
@@ -1,35 +1,11 @@
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name = "nginx-alb"
-
+resource "aws_lb" "dev_alb" {
+  name               = "dev_alb"
+  internal           = false
   load_balancer_type = "application"
-
-  vpc_id             = module.dev_vpc.vpc_id
-  subnets            = module.dev_vpc.public_subnets
   security_groups    = [aws_security_group.allow_tls.id]
+  subnets            = [for subnet in module.dev_vpc.public : subnet.id]
 
-  access_logs = {
-    bucket = "dev-alb-logs"
-  }
-
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = "arn:aws:acm:ap-northeast-1:312490145519:certificate/684674c1-deee-449f-a8cc-7f5484452e1f"
-      target_group_index = 0
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  enable_deletion_protection = true
 
   tags = {
     Terraform = "true"
