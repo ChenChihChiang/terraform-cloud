459bc05ac78f146168add706ad20f3ce6a2270ce
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 21:10:13 +0800

    t

--- route53.tf
+++ route53.tf
@@ -15,3 +15,17 @@ resource "aws_route53_record" "test" {
   ttl     = "300"
   records = ["1.1.1.1"]
 }
+
+
+resource "aws_route53_record" "dev_nginx" {
+  zone_id = aws_route53_zone.dev_chenchihchiang_tw.zone_id
+  name    = "nginx.dev.chenchihchiang.tw"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.dev_alb.dns_name
+    zone_id                = aws_lb.dev_alb.zone_id
+    evaluate_target_health = true
+  }
+
+}
