0b572d78902888945c7d018823f26fc6399b279a
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 16:03:28 +0800

    t

--- task_definition.tf
+++ task_definition.tf
@@ -11,7 +11,7 @@ resource "aws_ecs_task_definition" "nginx" {
     "image": "library/nginx",
     "cpu": 1024,
     "memory": 2048,
-    "essential": true
+    "essential": true,
       portMappings = [
         {
           containerPort = 80
