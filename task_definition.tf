f32ffb660382722f3bc96ba783c991c4d234f1cd
Chih Chiang Chen <chihchinag@gmail.com>
2021-12-31 20:06:50 +0800

    t

--- task_definition.tf
+++ task_definition.tf
@@ -2,16 +2,16 @@ resource "aws_ecs_task_definition" "nginx" {
   family                   = "nginx"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 1
+  memory                   = 1024
   task_role_arn            = "arn:aws:iam::312490145519:role/ecsTaskExecutionRole"
   execution_role_arn       = "arn:aws:iam::312490145519:role/ecsTaskExecutionRole"
   container_definitions    = <<TASK_DEFINITION
 [
   {
     "name": "nginx",
-    "image": "312490145519.dkr.ecr.ap-northeast-1.amazonaws.com/nginx:latest",
-    "cpu": 512,
+    "image": "docker.io/library/nginx:latest",
+    "cpu": 1,
     "memory": 1024,
     "essential": true,
     "portMappings": [
