58d9a46c9a6d4b7f0925a0ac24aa263c839d46d8
Chih Chiang Chen <chihchinag@gmail.com>
2022-01-02 15:19:43 +0800

    tedt

---
+++ dns_record.tf
@@ -0,0 +1,20 @@
+resource "aws_route53_record" "test" {
+  zone_id = aws_route53_zone.dev_chenchihchiang_tw.zone_id
+  name    = "test.dev.chenchihchiang.tw"
+  type    = "A"
+  ttl     = "300"
+  records = ["1.1.1.1"]
+}
+
+resource "aws_route53_record" "dev_nginx" {
+  zone_id = aws_route53_zone.dev_chenchihchiang_tw.zone_id
+  name    = "nginx.dev.chenchihchiang.tw"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.dev_alb.dns_name
+    zone_id                = aws_lb.dev_alb.zone_id
+    evaluate_target_health = true
+  }
+
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
-variable AWS_ACCESS_KEY_ID {}
+variable AWS_ACCESS_KEY_ID     {}
 variable AWS_SECRET_ACCESS_KEY {}
-variable AWS_DEFAULT_REGION  {}
+variable AWS_DEFAULT_REGION    {}
+variable GODADDY_KEY           {}
+variable GODADDY_SECRET        {}
